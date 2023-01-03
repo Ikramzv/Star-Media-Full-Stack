229b68998c29f63d6d8e49346d9fdeda830cb50c
Ikram <ikramzulfugar@gmail.com>
2023-01-03 21:09:28 +0400

    LIKE POST and SET_ADDITIONAL_POSTS_TO_SHOW added

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,31 +1,49 @@
-import { CREATE_POST, DELETE_POST, GET_USER, LIKE_POST, SET_TIMELINE_POSTS } from "../actions/actionTypes"
+import {
+  CREATE_POST,
+  DELETE_POST,
+  LIKE_POST,
+  SET_ADDITIONAL_POSTS_TO_SHOW,
+  SET_TIMELINE_POSTS,
+} from "../actions/actionTypes";
 
-export default (state = { posts: [] , postUsers: [] }, action) => {
-    switch (action.type) {
-        case SET_TIMELINE_POSTS: 
+export default (state = { posts: [], postUsers: [] }, action) => {
+  switch (action.type) {
+    case SET_TIMELINE_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+      };
+    case SET_ADDITIONAL_POSTS_TO_SHOW:
+      return {
+        ...state,
+        posts: [...state.posts, ...action.payload],
+      };
+    case LIKE_POST:
+      return {
+        ...state,
+        posts: state.posts.map((post) => {
+          if (post._id === action.postId) {
             return {
-                ...state,
-                posts: action.payload
-            }
-        case LIKE_POST:
-            return {
-                ...state , 
-                posts: state.posts.map((post) => post._id === action.payload._id ? action.payload : post)
-            }
-        case GET_USER: 
-            state.postUsers.push(action.payload)
-            return state
-        case CREATE_POST: 
-            return {
-                ...state,
-                posts: [...state.posts,action.payload]
-            }
-        case DELETE_POST: 
-            return {
-                ...state,
-                posts: state.posts.filter(post => post?._id !== action.payload)
-            }
-        default: 
-            return state
-    }
-}
+              ...post,
+              likes: post.likes.some((id) => id === action.userId)
+                ? post.likes.filter((id) => id !== action.userId)
+                : [...post.likes, action.userId],
+            };
+          }
+          return post;
+        }),
+      };
+    case CREATE_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts],
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post?._id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
