8174f26d0fe8b56908fb4cb8f21a6991debe6b73
Ikram <ikramzulfugar@gmail.com>
2023-01-06 01:47:37 +0400

    url fixed

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 const __prod__ = process.env.NODE_ENV === "production";
 
-const SERVER_URL = __prod__
-  ? process.env.REACT_PUBLIC_SERVER_URL
-  : "http://localhost:5000";
+// const SERVER_URL = __prod__
+//   ? "https://star-media.onrender.com"
+//   : "http://localhost:5000";
+
+const SERVER_URL = "https://star-media.onrender.com";
 
 export default SERVER_URL;
