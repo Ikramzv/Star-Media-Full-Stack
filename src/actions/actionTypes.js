c9c8ac37d5659cf6aaf84d8e15901cdb3d741af5
Ikram <ikramzulfugar@gmail.com>
2023-01-05 04:21:21 +0400

    update post type added

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,14 +1,17 @@
 export const SET_USER = "SET_USER";
+export const GET_USER = "GET_USER";
+export const FOLLOW_USER = "FOLLOW_USER";
+export const SET_PROFILE_USER = "SET_PROFILE_USER";
+
 export const SET_TIMELINE_POSTS = "SET_TIMELINE_POSTS";
 export const SET_ADDITIONAL_POSTS_TO_SHOW = "SET_ADDITIONAL_POSTS_TO_SHOW";
 export const LOGOUT = "LOGOUT";
 export const LIKE_POST = "LIKE_POST";
-export const GET_USER = "GET_USER";
 export const START_LOADING = "START_LOADING";
 export const END_LOADING = "END_LOADING";
 export const CREATE_POST = "CREATE_POST";
-export const FOLLOW_USER = "FOLLOW_USER";
 export const DELETE_POST = "DELETE_POST";
+export const UPDATE_POST = "UPDATE_POST";
 
 export const GET_CONVERSATION = "GET_CONVERSATION";
 export const CREATE_CONVERSATION = "CREATE_CONVERSATION";
