3711613991b8831553a10a6b43e63b8358511e28
Ikram <ikramzulfugar@gmail.com>
2023-01-05 04:20:11 +0400

    nothing changed

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,23 +1,23 @@
-import { LOGOUT, SET_USER } from "../actions/actionTypes"
+import { LOGOUT, SET_USER } from "../actions/actionTypes";
 const initialState = {
-    user: JSON.parse(localStorage.getItem('user')),
-}
+  user: JSON.parse(localStorage.getItem("user")),
+};
 
-export default (state = initialState , action) => {
-    switch (action.type) {
-        case SET_USER: 
-            localStorage.setItem('user' , JSON.stringify(action.payload))
-            return {
-                ...state,
-                user: action.payload
-            }
-        case LOGOUT: 
-            localStorage.removeItem('user')
-            return {
-                ...state,
-                user: null
-            }
-        default: 
-            return state
-    } 
-}
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER:
+      localStorage.setItem("user", JSON.stringify(action.payload));
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case LOGOUT:
+      localStorage.removeItem("user");
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
