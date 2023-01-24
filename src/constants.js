61a84528a1651ed397d5b98401cbf1ceefdc7dd9
Ikram <ikramzulfugar@gmail.com>
2023-01-24 05:12:14 +0400

    change server url based on node environment

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,7 @@
 const __prod__ = process.env.NODE_ENV === "production";
 
-// const SERVER_URL = __prod__
-//   ? "https://star-media.onrender.com"
-//   : "http://localhost:5000";
-
-const SERVER_URL = "https://star-media.onrender.com";
+const SERVER_URL = __prod__
+  ? "https://star-media.onrender.com"
+  : "http://localhost:5000";
 
 export default SERVER_URL;
