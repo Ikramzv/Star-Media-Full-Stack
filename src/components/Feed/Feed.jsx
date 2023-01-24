8ca242128440afb7c89397994308cda37cc71b02
Ikram <ikramzulfugar@gmail.com>
2023-01-24 05:13:55 +0400

    removed getProfilePosts here

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 
-import { getProfilePostsAction } from "../../actions/postAction";
 import Post from "../Post/Post";
 import Share from "../Share/Share";
 import "./Feed.css";
@@ -9,27 +8,14 @@ import Wrapper from "./Wrapper";
 
 function Feed({ isProfilePage, profileUser }) {
   const { posts } = useSelector((state) => state.posts);
-  const dispatch = useDispatch();
-
-  async function getProfilePosts() {
-    dispatch(getProfilePostsAction(profileUser?._id));
-  }
-
-  useEffect(() => {
-    if (isProfilePage) {
-      getProfilePosts();
-    }
-  }, [profileUser?._id]);
 
   return (
     <div className="feed">
       <Wrapper posts={posts} isProfile={{ isProfilePage, profileUser }}>
         <Share />
-        {posts
-          .sort((a, b) => b.createdAt - a.createdAt)
-          .map((post) => (
-            <Post post={post} key={post._id} />
-          ))}
+        {posts.map((post) => (
+          <Post post={post} key={post._id} />
+        ))}
       </Wrapper>
     </div>
   );
