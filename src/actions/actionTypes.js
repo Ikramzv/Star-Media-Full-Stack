5e590a74632c5328b9ba1a2db858e52bd7668ada
Ikram <ikramzulfugar@gmail.com>
2023-01-30 02:34:43 +0400

    action types well commented acc to their names

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,24 +1,39 @@
+// user
 export const SET_USER = "SET_USER";
 export const GET_USER = "GET_USER";
-export const FOLLOW_USER = "FOLLOW_USER";
-export const SET_PROFILE_USER = "SET_PROFILE_USER";
+export const LOGOUT = "LOGOUT";
 export const SET_PROFILE_POSTS = "SET_PROFILE_POSTS";
 
-export const LOGOUT = "LOGOUT";
+// loading
+export const START_LOADING = "START_LOADING";
+export const END_LOADING = "END_LOADING";
+
+// comment loading
+
+export const START_C_LOADING = "START_C_LOADING";
+export const END_C_LOADING = "END_C_LOADING";
+
+// Modal
+
+export const SET_MODAL = "SET_MODAL";
+export const ON_CLOSE = "ON_CLOSE";
+
+// posts
+
 export const LIKE_POST = "LIKE_POST";
 export const SET_ADDITIONAL_POSTS_TO_SHOW = "SET_ADDITIONAL_POSTS_TO_SHOW";
 export const SET_TIMELINE_POSTS = "SET_TIMELINE_POSTS";
-export const START_LOADING = "START_LOADING";
-export const END_LOADING = "END_LOADING";
 export const CREATE_POST = "CREATE_POST";
 export const DELETE_POST = "DELETE_POST";
 export const UPDATE_POST = "UPDATE_POST";
 export const INCREMENT_COMMENT_COUNTS = "INCREMENT_COMMENT_COUNTS";
 export const DECREMENT_COMMENT_COUNTS = "DECREMENT_COMMENT_COUNTS";
 
+// Conversations
 export const GET_CONVERSATIONS = "GET_CONVERSATIONS";
 export const CREATE_CONVERSATION = "CREATE_CONVERSATION";
 
+// Comments
 export const CREATE_COMMENT = "CREATE_COMMENT";
 export const GET_COMMENTS = "GET_COMMENTS";
 export const EDIT_COMMENT = "EDIT_COMMENT";
@@ -26,6 +41,8 @@ export const DELETE_COMMENT = "DELETE_COMMENT";
 export const LIKE_COMMENT = "LIKE_COMMENT";
 
 export const FETCH_FROM_CACHE = "FETCH_FROM_CACHE";
+
+// Messages
 
 export const GET_MESSAGES = "GET_MESSAGES";
 export const SEND_MESSAGE = "SEND_MESSAGE";
