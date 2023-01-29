6fd4fc0e8da0fa95e75aa4472d26e33ed2f1b411
Ikram <ikramzulfugar@gmail.com>
2023-01-30 02:42:16 +0400

    .

--- src/reducers/loadingReducer.js
+++ src/reducers/loadingReducer.js
@@ -1,12 +1,12 @@
-import { END_LOADING, START_LOADING } from "../actions/actionTypes"
+import { END_LOADING, START_LOADING } from "../actions/actionTypes";
 
-export default (loading = false , action) => {
-    switch (action.type) {
-        case START_LOADING: 
-            return loading = true
-        case END_LOADING: 
-            return loading = false
-        default: 
-            return loading
-    }
-}
+export default (loading = false, action) => {
+  switch (action.type) {
+    case START_LOADING:
+      return (loading = true);
+    case END_LOADING:
+      return (loading = false);
+    default:
+      return loading;
+  }
+};
