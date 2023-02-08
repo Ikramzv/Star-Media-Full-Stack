bedea3c9f02869d8656607ec6db00b80bd924263
Ikram <ikramzulfugar@gmail.com>
2023-02-09 00:31:13 +0400

    export socket from constants

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,14 @@
+import io from "socket.io-client";
+
 const __prod__ = process.env.NODE_ENV === "production";
 
 const SERVER_URL = __prod__
   ? "https://star-media.onrender.com"
   : "http://localhost:5000";
+
+export const socket = io(SERVER_URL, {
+  transports: ["websocket"],
+  autoConnect: false,
+});
 
 export default SERVER_URL;
