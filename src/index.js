4b25df0b2309a7ca762a2599047ca2e5969bc853
Ikram <ikramzulfugar@gmail.com>
2023-02-18 03:03:06 +0400

    store extracerd to own file

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
 import { BrowserRouter as Router } from "react-router-dom";
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
 import App from "./App";
 import "./index.css";
-import reducers from "./reducers/index";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const store = createStore(reducers, applyMiddleware(thunk));
 
 root.render(
   <Provider store={store}>
