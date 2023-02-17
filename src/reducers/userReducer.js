86b6cde55d4aed4e464a3af763cfc10c9a39d17f
Ikram <ikramzulfugar@gmail.com>
2023-02-18 02:57:46 +0400

    user slice created

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
 import { LOGOUT, SET_USER } from "../actions/actionTypes";
 const initialState = {
   user: JSON.parse(localStorage.getItem("user")),
 };
+
+const userSlice = createSlice({
+  initialState: JSON.parse(localStorage.getItem("user")),
+  name: "user",
+  reducers: {
+    setUser: (state, action) => {
+      const user = action.payload;
+      localStorage.setItem("user", JSON.stringify(user));
+      return user;
+    },
+    logOut: (state, action) => {
+      localStorage.removeItem("user");
+      return null;
+    },
+  },
+});
+
+export const { reducer } = userSlice;
+export const { logOut, setUser } = userSlice.actions;
 
 export default (state = initialState, action) => {
   switch (action.type) {
