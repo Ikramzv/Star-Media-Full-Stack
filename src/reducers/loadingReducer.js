38a35eeb28467a38579d01b244460aabef9399fb
Ikram <ikramzulfugar@gmail.com>
2023-02-18 02:57:54 +0400

    loading slice created

--- src/reducers/loadingReducer.js
+++ src/reducers/loadingReducer.js
@@ -1,4 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
 import { END_LOADING, START_LOADING } from "../actions/actionTypes";
+
+const loadingSlice = createSlice({
+  initialState: false,
+  name: "loading",
+  reducers: {
+    setLoading: (state, action) => action.payload,
+  },
+});
+
+export const { reducer } = loadingSlice;
+export const { setLoading } = loadingSlice.actions;
 
 export default (loading = false, action) => {
   switch (action.type) {
