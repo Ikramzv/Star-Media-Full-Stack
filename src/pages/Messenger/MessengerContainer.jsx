bedc65eef47ffc12298c1e25df6b36f6ebcbca55
Ikram <ikramzulfugar@gmail.com>
2023-02-26 16:54:09 +0400

    fetch messages with rtk query

--- src/pages/Messenger/MessengerContainer.jsx
+++ src/pages/Messenger/MessengerContainer.jsx
@@ -1,12 +1,36 @@
-import React, { useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useMemo, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { injectEndpoints } from "../../api";
+import { setMessages } from "../../slices/messagesReducer";
 
 function MessengerContainer({ children }) {
-  const { messages } = useSelector((state) => state.messages);
+  const messages = useSelector((state) => state.messages);
+  const dispatch = useDispatch();
+  const { id: convId } = useParams();
   const box = useRef();
+
+  const { useLazyGetMessagesQuery } = useMemo(() => {
+    return injectEndpoints({
+      endpoints: (builder) => ({
+        getMessages: builder.query({
+          query: (convId) => `/messages/${convId}`,
+        }),
+      }),
+    });
+  }, []);
+
+  const [getMessages] = useLazyGetMessagesQuery();
+  useEffect(() => {
+    if (convId) {
+      getMessages(convId, true).then(({ data }) => dispatch(setMessages(data)));
+    }
+  }, [convId]);
+
   useEffect(() => {
     box.current && (box.current.scrollTop = box.current.scrollHeight);
   }, [messages.length]);
+
   return (
     <div className="chatBoxTop" ref={box}>
       {children}
@@ -14,4 +38,4 @@ function MessengerContainer({ children }) {
   );
 }
 
-export default MessengerContainer;
+export default React.memo(MessengerContainer);
