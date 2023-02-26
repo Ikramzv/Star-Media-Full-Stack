342844b8b1aa118d47f303c8e3b39be40d944069
Ikram <ikramzulfugar@gmail.com>
2023-02-26 14:31:11 +0400

    reducers added

--- src/store.js
+++ src/store.js
@@ -1,13 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./api/index";
-import { reducer as loadingReducer } from "./reducers/loadingReducer";
-import { reducer as userReducer } from "./reducers/userReducer";
+import {
+  commentsReducer,
+  currentChatReducer,
+  loadingReducer,
+  messagesReducer,
+  modalReducer,
+  onlineFriendsReducer,
+  userReducer,
+} from "./slices";
 
 const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     user: userReducer,
     loading: loadingReducer,
+    modal: modalReducer,
+    comments: commentsReducer,
+    currentChat: currentChatReducer,
+    messages: messagesReducer,
+    onlineFriends: onlineFriendsReducer,
   },
   middleware: (gdm) => {
     return gdm().concat([api.middleware]);
