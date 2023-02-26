89418f7d392b6ec63ff76b95ba9a5a03f167294b
Ikram <ikramzulfugar@gmail.com>
2023-02-26 16:54:41 +0400

    online friends managing migrated to App component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,26 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Feed from "../../components/Feed/Feed";
 import Rightbar from "../../components/Rightbar/Rightbar";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import { socket } from "../../constants";
 import "./home.css";
 
 function Home() {
-  const { user } = useSelector((state) => state);
+  const user = useSelector((state) => state.user);
   const navigate = useNavigate();
-  const [onlineFriends, setOnlineFriends] = useState([]);
 
   useEffect(() => {
     if (!user) {
       navigate("/login");
-    } else if (socket && user) {
-      socket.emit("sendUser", user?._id);
-      socket.on("getUsers", (users) => {
-        const onlines = users.filter((u) => u.userId !== user?._id);
-        setOnlineFriends(onlines);
-      });
     }
   }, [user]);
 
@@ -29,7 +21,7 @@ function Home() {
       <div className="homeContainer">
         <Sidebar />
         <Feed />
-        <Rightbar onlineFriends={onlineFriends} />
+        <Rightbar />
       </div>
     </div>
   );
