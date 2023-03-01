c1b1f9eaf3826e0f3466611ecfb7184243e07d06
Ikram <ikramzulfugar@gmail.com>
2023-03-01 19:43:49 +0400

    socket logic exported to Socket component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { CircularProgress } from "@mui/material";
-import React, { useEffect, useMemo } from "react";
-import { useDispatch } from "react-redux";
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
-import { socket } from "./constants";
+import React, { useMemo } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+import RealtimeSocketMessages from "./components/Socket/Socket";
 import withStore from "./hocs/withStore";
 import HomeLayout from "./Layout/HomeLayout";
 import Home from "./pages/Home/Home";
@@ -10,41 +10,37 @@ import Login from "./pages/Login/Login";
 import Mesenger from "./pages/Messenger/Mesenger";
 import Profile from "./pages/Profile/Profile";
 import Register from "./pages/Register/Register";
-import { setOnlineFriends } from "./slices/onlineFriendsSlice";
 
 function App({ state }) {
-  const { user, loading } = useMemo(() => state, Object.values(state));
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (!user) {
-      navigate("/login");
-      socket.close();
-    } else {
-      const s = socket.connect();
-      s.emit("sendUser", user?._id);
-      s.on("getUsers", (users) => {
-        dispatch(setOnlineFriends(users));
-      });
-    }
-  }, [user]);
+  const { loading, user } = useMemo(() => state, Object.values(state));
+
   return (
     <div style={{ overflowX: "clip" }}>
-      {loading && <CircularProgress color="error" className="loading" />}
-      <Routes>
-        <Route
-          path="/"
-          element={<Navigate replace={"/"} to={user ? "/home" : "/login"} />}
-        />
-        <Route path="/" element={<HomeLayout />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:id" element={<Profile />} />
-          <Route path="/messenger" element={<Mesenger />} />
-          <Route path="/messenger/:id" element={<Mesenger />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <Auth>
+        <RealtimeSocketMessages>
+          {loading ? (
+            <CircularProgress color="error" className="loading" />
+          ) : (
+            ""
+          )}
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Navigate replace={"/"} to={user ? "/home" : "/login"} />
+              }
+            />
+            <Route path="/" element={<HomeLayout />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/profile/:id" element={<Profile />} />
+              <Route path="/messenger" element={<Mesenger />} />
+              <Route path="/messenger/:id" element={<Mesenger />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </RealtimeSocketMessages>
+      </Auth>
     </div>
   );
 }
