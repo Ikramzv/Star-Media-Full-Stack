7985ac099c89410c1525fd03a9574b8242d11f37
Ikram <ikramzulfugar@gmail.com>
2023-03-01 19:45:46 +0400

    localStorage logic removed

--- src/slices/userReducer.js
+++ src/slices/userReducer.js
@@ -1,21 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-const initialState = {
-  user: JSON.parse(localStorage.getItem("user")),
-};
 
 const userSlice = createSlice({
-  initialState: JSON.parse(localStorage.getItem("user")),
+  initialState: null,
   name: "user",
   reducers: {
-    setUser: (state, action) => {
-      const user = action.payload;
-      localStorage.setItem("user", JSON.stringify(user));
-      return user;
-    },
-    logOut: (state, action) => {
-      localStorage.removeItem("user");
-      return null;
-    },
+    setUser: (state, action) => action.payload,
+    logOut: () => null,
   },
 });
 
